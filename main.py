b341e7c7a41f7c942ba15b13b579745fcf2d2733
bayar <BayarevichAleh@yandex.by>
2020-05-20 21:33:43 +0300

    сумма и факториал

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-a=int(input())
-b=int(input())
-print(a/b)
-
-
+i = 0
+result = 0
+while result <= 1000:
+    result = result + i;
+    i += 1
+print(result)
+i = int(input("Enter i:"))
+result = i
+while i > 1:
+    result = result * (i - 1)
+    i -= 1
+print(result)
