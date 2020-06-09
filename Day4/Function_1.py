8ac22d013f314da83bd1bb56de3b0cd2465c15d5
bayar <BayarevichAleh@yandex.by>
2020-06-09 11:00:30 +0300

    Функция, возвращающая среднее арифметическое двух аргументов

---
+++ Day4/Function_1.py
@@ -0,0 +1,13 @@
+def function_1(num_1, num_2):
+    """
+    Finding the arithmetic mean
+    :param num_1: number 1
+    :param num_2: number 2
+    :type num_1, num_2: float
+    :return: arithmetic mean
+    :return type: float
+    """
+    return (num_1 + num_2) / 2
+
+
+print(function_1(float(input("Enter number 1:")), float(input("Enter number 2:"))))
