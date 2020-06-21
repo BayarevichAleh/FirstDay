bd339e0890d472b351504a24f35bd413403931ba
SamirWardak <wardag.as@yahoo.com>
2020-06-21 11:17:46 +0300

    day8 changes

--- Day8/Home_work_3/home_work_3.py
+++ Day8/Home_work_3/home_work_3.py
@@ -12,16 +12,11 @@ with open('dates.json', 'w') as my_file_1:
     my_file_1.write(json.dumps(a))
 
 with open('dates.json', 'r') as my_file_1:
-    read_data = json.loads(my_file_1.read())
-
-get_date = list()
-for i in read_data:
-    j = i.split('-')
-    get_date.append(datetime.date(int(j[2]), int(j[1]), int(j[0])))
-
-
-min_date = min(get_date)
-print(min_date)
+    now = datetime.datetime.now()
+    for i in json.loads(my_file_1.read()):
+        if now > datetime.datetime.strptime(i, "%d-%m-%Y"):
+            now = datetime.datetime.strptime(i, "%d-%m-%Y")
+    print(now)
 
 
 
